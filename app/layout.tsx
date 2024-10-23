f6b64bc49ef55352a2c45444fb289c0b231f1de6
ruben-senpai11 <rubensenpai11@gmail.com>
2024-10-23 17:07:15 +0100

    g-tag ok

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,14 +16,22 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+    <head>
+      {/* External Google Analytics script */}
       <script async src="https://www.googletagmanager.com/gtag/js?id=G-XTLMQZ4GV3"></script>
-      <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
 
-        gtag('config', 'G-XTLMQZ4GV3');
-      </script>
+      {/* Inline Google Analytics setup script */}
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-XTLMQZ4GV3');
+          `,
+        }}
+      />
+    </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
