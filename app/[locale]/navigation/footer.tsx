64ee80e29584a9787a5b722b0654770af838d80d
ruben-senpai <albanhonfovou@gmail.com>
2025-01-04 11:49:04 +0100

    updated linkedin

--- app/[locale]/navigation/footer.tsx
+++ app/[locale]/navigation/footer.tsx
@@ -17,7 +17,7 @@ function Footer(){
           <p>Made with 🤍 by the white_dev</p>
         </div>
         <div className="socials flex gap-4 ">
-          <Link href="https://linkedin.com/in/alban-honfovou/" target="_blank" >LN</Link>
+          <Link href="https://www.linkedin.com/in/ruben-white22/" target="_blank" >LN</Link>
           <Link href="https://www.facebook.com/alban.honfovou/" target="_blank" >FA</Link>
           <Link href="https://wa.me/+22996404757/" target="_blank" >WA</Link>
           <Link href="https://github.com/ruben-senpai11" target="_blank" >GH</Link>
