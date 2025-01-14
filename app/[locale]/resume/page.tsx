a888b2605ebd4be890bb2a032d669348e02f0ece
ruben-senpai11 <rubensenpai11@gmail.com>
2025-01-14 09:35:21 +0100

    final ?

--- app/[locale]/resume/page.tsx
+++ app/[locale]/resume/page.tsx
@@ -2,6 +2,7 @@ import BBorder from "../bborder";
 import LoadingGif from "../components/loadingGif";
 
 
+
 const Page = () => {
   const pdfUrl = '/Ruben Resume 2025.pdf'; 
 
@@ -580,27 +581,15 @@ const Page = () => {
     </div>
   </div>
 </div>`;
+
+
   return (
     <>
     {/* <BBorder> */}
-      {/* <div className="w-[100%]">
-        <div style={{ width: '100%', height: '100vh' }}>
-          <iframe
-            src={pdfUrl}
-            width="100%"
-            height="100%"
-            style={{ border: 'none' }}
-            allow="autoplay"
-            title="White dev's Resume "
-          />
-        </div>
-      </div > */}
-
 
       <div className="flex [w-100%] [h-100%] ">
         <div className="flex justify-center items-center m-auto "dangerouslySetInnerHTML={{ __html: htmlContent }} />
       </div>
-
 
 
     {/* </BBorder > */}
