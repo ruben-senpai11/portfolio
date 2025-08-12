1e6fb7c12ac13190e9ca28982ce504afcdee394d
ruben-senpai11 <rubensenpai11@gmail.com>
2025-08-12 14:30:55 +0100

    fix: GTM issue fixed

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -5,12 +5,13 @@ import { getMessages } from 'next-intl/server';
 import { routing } from '@/i18n/routing';
 import { notFound } from 'next/navigation';
 import "../globals.css";
+import Script from "next/script";
 
-const inter = Inter({ subsets: ["latin"] });
+// const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "White dev Portfolio",
-  description: "Hi, I'm Ruben White. Frontend developer, though keep in mind I got a divine way to code !",
+  description: "Hi, I'm Rubene HONFOVOU, a UIX developer. Here is a chance to discover my work and know how helpful I can be to you !",
 };
 
 
@@ -28,22 +29,31 @@ export default async function LocaleLayout({
   return (
     <html lang={locale}>
       <head>
-        <!-- Google Tag Manager -->
-        <script>(function(w,d,s,l,i){w[l] = w[l] || [];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-T9MMWLWS');</script>
-        <!-- End Google Tag Manager -->
+        <Script
+          id="gtm-head"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id=GTM-T9MMWLWS'+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-T9MMWLWS');`,
+          }}
+        />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@200..800&family=Sora:wght@100..800&display=swap" rel="stylesheet" />
       </head>
-      <body className={inter.className}>
-        <!-- Google Tag Manager (noscript) -->
-        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T9MMWLWS"
-          height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-        <!-- End Google Tag Manager (noscript) -->
+      {/* <body className={inter.className}> */}
+      <body >
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-T9MMWLWS"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
         <NextIntlClientProvider messages={messages}>
           {children}
         </NextIntlClientProvider>
