5b013022a5e013fb53c4b6a31c90bfd6f50f8780
ruben-senpai11 <rubensenpai11@gmail.com>
2025-08-12 14:03:03 +0100

    feat: GTM

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -28,11 +28,22 @@ export default async function LocaleLayout({
   return (
     <html lang={locale}>
       <head>
+        <!-- Google Tag Manager -->
+        <script>(function(w,d,s,l,i){w[l] = w[l] || [];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-T9MMWLWS');</script>
+        <!-- End Google Tag Manager -->
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
-        <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@200..800&family=Sora:wght@100..800&display=swap" rel="stylesheet"/>
+        <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@200..800&family=Sora:wght@100..800&display=swap" rel="stylesheet" />
       </head>
       <body className={inter.className}>
+        <!-- Google Tag Manager (noscript) -->
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T9MMWLWS"
+          height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        <!-- End Google Tag Manager (noscript) -->
         <NextIntlClientProvider messages={messages}>
           {children}
         </NextIntlClientProvider>
