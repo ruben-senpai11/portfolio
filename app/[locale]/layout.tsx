5524647b44d94940d4dbee096fcc95249febb242
ruben-senpai11 <rubensenpai11@gmail.com>
2025-08-16 04:59:44 +0100

    fix: tag Id

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -38,7 +38,7 @@ export default async function LocaleLayout({ children, params }: LayoutProps) {
             __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
               new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
               j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id=GTM-T9MMWLWS'+dl;f.parentNode.insertBefore(j,f);
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
               })(window,document,'script','dataLayer','GTM-T9MMWLWS');`,
           }}
         />
@@ -47,15 +47,8 @@ export default async function LocaleLayout({ children, params }: LayoutProps) {
         <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@200..800&family=Sora:wght@100..800&display=swap" rel="stylesheet" />
       </head>
       <body className={inter.className}>
-      {/* <body > */}
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-T9MMWLWS"
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          ></iframe>
-        </noscript>
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T9MMWLWS"
+          height="0" width="0" style={{ display: "none", visibility: "hidden" }}></iframe></noscript>
         <NextIntlClientProvider messages={messages}>
           {children}
         </NextIntlClientProvider>
