7e2d56ae6e1b0c2f14ac9a940190175e3e07c444
ruben-senpai11 <rubensenpai11@gmail.com>
2025-08-16 04:47:41 +0100

    fix: GTM bug fixed

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -6,22 +6,24 @@ import { routing } from '@/i18n/routing';
 import { notFound } from 'next/navigation';
 import "../globals.css";
 import Script from "next/script";
+import { ReactNode } from "react";
 
-// const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"] });
+
+
+interface LayoutProps {
+  children: ReactNode;
+  params: Promise<{ locale: string }>;
+}
+
 
 export const metadata: Metadata = {
-  title: "White dev Portfolio",
-  description: "Hi, I'm Rubene HONFOVOU, a UIX developer. Here is a chance to discover my work and know how helpful I can be to you !",
+  title: "UIX developer | Rubene Honfovou Portfolio",
+  description: "Hi, I'm Rubene Honfovou, a UIX developer. Here is a chance to discover my work and know how helpful I can be to you !",
 };
 
 
-export default async function LocaleLayout({
-  children,
-  params,
-}: {
-  children: React.ReactNode;
-  params: { locale: string };
-}) {
+export default async function LocaleLayout({ children, params }: LayoutProps) {
 
   const { locale } = await Promise.resolve(params); // Ensure params is awaited if required by Next.js
   const messages = await getMessages();
@@ -44,8 +46,8 @@ export default async function LocaleLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@200..800&family=Sora:wght@100..800&display=swap" rel="stylesheet" />
       </head>
-      {/* <body className={inter.className}> */}
-      <body >
+      <body className={inter.className}>
+      {/* <body > */}
         <noscript>
           <iframe
             src="https://www.googletagmanager.com/ns.html?id=GTM-T9MMWLWS"
